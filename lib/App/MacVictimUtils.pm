263e0afdf22fb8f9b4badec9a77acdb1d5ea4de3
perlancar (on pc-office) <perlancar@gmail.com>
2017-09-20 19:50:22 +0700

    Initial commit

---
+++ lib/App/MacVictimUtils.pm
@@ -0,0 +1,62 @@
+package App::MacVictimUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::ger;
+
+our %SPEC;
+
+$SPEC{delete_ds_store} = {
+    v => 1.1,
+    summary => 'Recursively delete .DS_Store files',
+    args => {
+        dirs => {
+            schema => ['array*', of=>'dirname*'],
+            pos => 0,
+            greedy => 1,
+        },
+    },
+    features => {
+        dry_run => 1,
+    },
+};
+sub delete_ds_store {
+    require File::Find;
+
+    my %args = @_;
+
+    my @dirs = @{ $args{dirs} || ["."] };
+
+    File::Find::find(
+        sub {
+            next unless -f && $_ eq '.DS_Store';
+            if ($args{-dry_run}) {
+                log_info("[DRY] Deleting %s/.DS_Store ...", $File::Find::dir);
+                return;
+            }
+            log_info("Deleting %s/.DS_Store ...", $File::Find::dir);
+            unlink $_ or do {
+                log_warn("Can't delete %s/.DS_Store: %s", $File::Find::dir, $!);
+            };
+        },
+        @dirs,
+    );
+}
+
+1;
+#ABSTRACT: CLI utilities for when dealing with Mac computers/files
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
